d74f1c3849582e10b7a1fa5eac69c22dce4e3eaf
ritesh-chaudhari-aepl <techteam@urdoer.com>
2023-06-16 11:51:09 +0530

    latest change

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Cookies from "./common/Cookies";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <>
+      <Cookies />
+      <Home />
+    </>
+  );
+};
+
+export default App;
